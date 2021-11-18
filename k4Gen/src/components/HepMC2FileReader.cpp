91f85637e7fb9dfcf4b038c4506ffc74b996eec3
Valentin Volkl <valentin.volkl@cern.ch>
2021-11-18 18:14:16 +0100

    hepmc3: transition complete

--- k4Gen/src/components/HepMC2FileReader.cpp
+++ k4Gen/src/components/HepMC2FileReader.cpp
@@ -1,43 +1,39 @@
 
-#include "HepMCFileReaderTool.h"
+#include "HepMC2FileReader.h"
 
 #include "GaudiKernel/IEventProcessor.h"
 #include "GaudiKernel/IIncidentSvc.h"
 #include "GaudiKernel/Incident.h"
 
-DECLARE_COMPONENT(HepMCFileReader)
+DECLARE_COMPONENT(HepMC2FileReader)
 
-HepMCFileReader::HepMCFileReader(const std::string& type, const std::string& name, const IInterface* parent)
+HepMC2FileReader::HepMC2FileReader(const std::string& type, const std::string& name, const IInterface* parent)
     : GaudiTool(type, name, parent), m_file(nullptr) {
   declareInterface<IHepMCProviderTool>(this);
 }
 
-HepMCFileReader::~HepMCFileReader() { ; }
+HepMC2FileReader::~HepMC2FileReader() { ; }
 
-StatusCode HepMCFileReader::initialize() {
+StatusCode HepMC2FileReader::initialize() {
   if (m_filename.empty()) {
     error() << "Input file name is not specified!" << endmsg;
     return StatusCode::FAILURE;
   }
   // open file using HepMC routines
-  m_file = std::make_unique<HepMC3::ReaderAscii>(m_filename.value());
+  m_file = std::make_unique<HepMC3::ReaderAsciiHepMC2>(m_filename.value());
   StatusCode sc = GaudiTool::initialize();
   return sc;
 }
 
-StatusCode HepMCFileReader::getNextEvent(HepMC3::GenEvent& event) {
+StatusCode HepMC2FileReader::getNextEvent(HepMC3::GenEvent& event) {
   if (!m_file->read_event(event)) {
-   // if (m_file->rdstate() == std::ios::eofbit) {
-   //   error() << "Error reading HepMC file" << endmsg;
-   //   return StatusCode::FAILURE;
-   // }
     error() << "Premature end of file: Please set the number of events according to hepMC file." << endmsg;
     return Error("Reached end of file before finished processing");
   }
   return StatusCode::SUCCESS;
 }
 
-StatusCode HepMCFileReader::finalize() {
+StatusCode HepMC2FileReader::finalize() {
   m_file.reset();
   return GaudiTool::finalize();
 }
---
+++ k4Gen/src/components/HepMCFileReader.cpp
@@ -0,0 +1,39 @@
+
+#include "HepMCFileReader.h"
+
+#include "GaudiKernel/IEventProcessor.h"
+#include "GaudiKernel/IIncidentSvc.h"
+#include "GaudiKernel/Incident.h"
+
+DECLARE_COMPONENT(HepMCFileReader)
+
+HepMCFileReader::HepMCFileReader(const std::string& type, const std::string& name, const IInterface* parent)
+    : GaudiTool(type, name, parent), m_file(nullptr) {
+  declareInterface<IHepMCProviderTool>(this);
+}
+
+HepMCFileReader::~HepMCFileReader() { ; }
+
+StatusCode HepMCFileReader::initialize() {
+  if (m_filename.empty()) {
+    error() << "Input file name is not specified!" << endmsg;
+    return StatusCode::FAILURE;
+  }
+  // open file using HepMC routines
+  m_file = std::make_unique<HepMC3::ReaderAscii>(m_filename.value());
+  StatusCode sc = GaudiTool::initialize();
+  return sc;
+}
+
+StatusCode HepMCFileReader::getNextEvent(HepMC3::GenEvent& event) {
+  if (!m_file->read_event(event)) {
+    error() << "Premature end of file: Please set the number of events according to hepMC file." << endmsg;
+    return Error("Reached end of file before finished processing");
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode HepMCFileReader::finalize() {
+  m_file.reset();
+  return GaudiTool::finalize();
+}
