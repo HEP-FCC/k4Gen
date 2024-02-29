a07a1b78938eaff05c23482a7bf0931dbf2c35f9
Juraj Smiesko <juraj.smiesko@cern.ch>
2024-02-29 15:12:51 +0100

    Saving filter stats to file

--- k4Gen/src/components/GenEventFilter.h
+++ k4Gen/src/components/GenEventFilter.h
@@ -1,6 +1,9 @@
 #ifndef GENERATION_GENEVENTFILTER_H
 #define GENERATION_GENEVENTFILTER_H
 
+// std
+#include <cstdint>
+
 // Gaudi
 #include "GaudiKernel/Algorithm.h"
 class IProperty;
@@ -9,6 +12,7 @@ class IEventProcessor;
 
 // k4FWCore
 #include "k4FWCore/DataHandle.h"
+#include "k4FWCore/MetaDataHandle.h"
 
 // Datamodel
 namespace edm4hep {
@@ -26,34 +30,37 @@ namespace edm4hep {
 class GenEventFilter : public Gaudi::Algorithm {
 
 public:
-  /// Constructor
+  /// Constructor.
   GenEventFilter(const std::string& name, ISvcLocator* svcLoc);
-  /// Initialize
+  /// Initialize.
   virtual StatusCode initialize();
-  /// Execute: Applies the filter
+  /// Execute: Applies the filter.
   virtual StatusCode execute(const EventContext& evtCtx) const;
-  /// Finalize
+  /// Finalize.
   virtual StatusCode finalize();
 
 private:
-  /// Handle for the MCParticle collection to be read
+  /// Handle for the MCParticle collection to be read.
   mutable DataHandle<edm4hep::MCParticleCollection> m_inColl{
       "particles", Gaudi::DataHandle::Reader, this};
+  /// Writes out filter statistics.
+  MetaDataHandle<std::vector<int>> m_evtFilterStats{
+      "EventFilterStats", Gaudi::DataHandle::Writer};
 
-  /// Rule to filter the events with
+  /// Rule to filter the events with.
   Gaudi::Property<std::string> m_filterRuleStr{
       this, "filterRule", "", "Filter rule to apply on the events"};
 
-  /// Path of the filter rule file
+  /// Path of the filter rule file.
   Gaudi::Property<std::string> m_filterRulePath{
       this, "filterRulePath", "", "Path to the filter rule file"};
 
   /// Targeted number of events.
-  mutable std::atomic<unsigned int> m_nEventsTarget;
+  mutable std::atomic<int> m_nEventsTarget;
   /// Keep track of how many events were already accepted.
-  mutable std::atomic<unsigned int> m_nEventsAccepted;
+  mutable std::atomic<int> m_nEventsAccepted;
   /// Keep track of how many events we went through.
-  mutable std::atomic<unsigned int> m_nEventsSeen;
+  mutable std::atomic<int> m_nEventsSeen;
   /// Pointer to the property.
   SmartIF<IProperty> m_property;
   /// Pointer to the incident service.
